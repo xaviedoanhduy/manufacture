4c95cd1dd52fa82a3a64e048ace3f45dc3967a2d
Arnau <arnau.cruz@forgeflow.com>
2025-07-29 15:30:47 +0200

    [MIG] mrp_production_putaway_strategy: Migration to 18.0

--- mrp_production_putaway_strategy/models/mrp_production.py
+++ mrp_production_putaway_strategy/models/mrp_production.py
@@ -7,22 +7,35 @@ from odoo import _, api, models
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    @api.model
-    def create(self, vals):
-        location_dest = self.env["stock.location"].browse(vals.get("location_dest_id"))
-        product = self.env["product.product"].browse(vals.get("product_id"))
-        location_id = location_dest._get_putaway_strategy(product)
-        if location_id:
-            vals["location_dest_id"] = location_id.id
-        mo = super().create(vals)
-        if location_id:
-            message = (
-                _(
-                    "Applied Putaway strategy to finished products.\n Finished "
-                    "Products Location: %s. "
-                )
-                % mo.location_dest_id.complete_name
-            )
+    @api.model_create_multi
+    def create(self, vals_list):
+        putaway_applied_flags = []
 
-            mo.message_post(body=message, message_type="comment")
-        return mo
+        for vals in vals_list:
+            product = self.env["product.product"].browse(vals.get("product_id"))
+            original_location = self.env["stock.location"].browse(
+                vals.get("location_dest_id")
+            )
+            putaway_location = original_location._get_putaway_strategy(product)
+            putaway_flag = False
+
+            if putaway_location.id != original_location.id:
+                vals["location_dest_id"] = putaway_location.id
+                putaway_flag = True
+
+            putaway_applied_flags.append(putaway_flag)
+
+        mos = super().create(vals_list)
+
+        for mo, applied in zip(mos, putaway_applied_flags, strict=False):
+            if applied:
+                mo.message_post(
+                    body=_(
+                        "Applied Putaway strategy to finished products.\n"
+                        "Finished Products Location: %s."
+                    )
+                    % mo.location_dest_id.complete_name,
+                    message_type="comment",
+                )
+
+        return mos
